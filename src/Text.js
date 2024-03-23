6baa939a20104d689e8db17242effb00753bd46a
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-23 19:30:23 +0530

    implimenting text file

---
+++ src/Text.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+
+class Text extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello world</h1>
+                <p>Welcome to javascript React project</p>
+            </div>
+        )
+    }
+}
+
+
+export default Text;
