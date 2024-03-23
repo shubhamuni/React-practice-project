60f53d4caab0eb40f558754d211ce1e76144ab70
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-23 19:57:57 +0530

    changes and deleted test file

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+// import Text from './Text'
+import './App.css';
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <img alt='robo' src='' />
+      <div>
+        <h2>Shubham Patil</h2>
+        <p>shubhampatil@example.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
