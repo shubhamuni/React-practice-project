60c2637c87ecb385c4877598e491001634b57671
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-23 20:12:11 +0530

    changed

--- src/Card.js
+++ src/Card.js
@@ -4,8 +4,8 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div>
-      <img alt='robo' src='' />
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='robo' src='https://robohash.org/test?200*200' />
       <div>
         <h2>Shubham Patil</h2>
         <p>shubhampatil@example.com</p>
