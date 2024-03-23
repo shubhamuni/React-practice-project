95b89f9025a7c9f9aadbc09c575747f992126c61
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-23 19:40:26 +0530

    class converted into arrow function

--- src/Text.js
+++ src/Text.js
@@ -1,15 +1,13 @@
-import React,{Component} from "react";
+import React from "react";
 
-class Text extends Component {
-    render(){
-        return (
-            <div className="f2 tc">
-                <h1>Hello world</h1>
-                <p>Welcome to javascript React project</p>
-                <p>{this.props.greeting}</p>
-            </div>
-        )
-    }
+const Text = (props) => {
+    return (
+        <div className="f2 tc">
+            <h1>Hello world</h1>
+            <p>Welcome to javascript React project</p>
+            <p>{props.greeting}</p>
+        </div>
+    )
 }
 
 
