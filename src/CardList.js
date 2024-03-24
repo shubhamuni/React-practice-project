c0c01ccd51427524a81dbb0c1b8c80ac32e3fc5e
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-24 14:46:19 +0530

    key prop added

--- src/CardList.js
+++ src/CardList.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Card from "./Card";
 
 
 export const CardList = (props) => {
     
-    const cardComponent = props.robots.map((user)=>{
-        return <Card name={user.name} id={user.id} email={user.email} />;
+    const cardArray = props.robots.map((user)=>{
+        return <Card key={user.id} name={user.name} id={user.id} email={user.email} />;
     })
     console.log(props.robots)
     return(
-        <div>
-            {cardComponent}
-        </div>    
+        <Fragment>
+            {cardArray}
+        </Fragment>    
     );
 }
