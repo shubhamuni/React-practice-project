750fea9b5e5b2a2a04520ab5fc0e6d23c32f41a5
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-24 17:50:20 +0530

    cardArray changed to return value

--- src/CardList.js
+++ src/CardList.js
@@ -4,13 +4,13 @@ import Card from "./Card";
 
 export const CardList = (props) => {
     
-    const cardArray = props.robots.map((user)=>{
-        return <Card key={user.id} name={user.name} id={user.id} email={user.email} />;
-    })
+    // const cardArray = props.robots.map((user)=>{
+    //     return <Card key={user.id} name={user.name} id={user.id} email={user.email} />;
+    // })
     console.log(props.robots)
     return(
-        <Fragment>
-            {cardArray}
-        </Fragment>    
+        props.robots.map((user)=>{
+            return <Card key={user.id} name={user.name} id={user.id} email={user.email} />;
+        })    
     );
 }
