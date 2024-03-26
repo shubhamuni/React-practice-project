a49598c14880d5ba64d40dccb69757f95bfa0145
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-26 20:26:53 +0530

    map() of two parameters

--- src/CardList.js
+++ src/CardList.js
@@ -9,7 +9,7 @@ export const CardList = ({ robots }) => {
     // })
     // console.log(robots)
     return(
-        robots.map((i)=>{
+        robots.map((user,i)=>{
             return <Card key={i} name={robots[i].name} id={robots[i].id} email={robots[i].email} />;
         })    
     );
