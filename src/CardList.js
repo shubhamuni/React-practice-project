f63f47042acbdaf2e76a2b88189af4a37bb6d511
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-26 20:27:12 +0530

    map() of two parameters

--- src/CardList.js
+++ src/CardList.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Card from "./Card";
 
+// const cardArray = props.robots.map((user)=>{
+//     return <Card key={user.id} name={user.name} id={user.id} email={user.email} />;
+// })
+// console.log(robots)
 
 export const CardList = ({ robots }) => {
     
-    // const cardArray = props.robots.map((user)=>{
-    //     return <Card key={user.id} name={user.name} id={user.id} email={user.email} />;
-    // })
-    // console.log(robots)
     return(
         robots.map((user,i)=>{
             return <Card key={i} name={robots[i].name} id={robots[i].id} email={robots[i].email} />;
