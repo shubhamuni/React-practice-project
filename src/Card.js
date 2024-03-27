299bd7773bee11fe92f1e80de77af44f49ee4c56
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-27 05:41:52 +0530

    bg-dark-green

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Card = (props) => {
  const { name, email, id } = props
   return (
-    <div className='bg-light-green dib br4 pa3 ma2 grow bw2 shadow-5'>
+    <div className='bg-dark-green dib br4 pa3 ma2 grow bw2 shadow-5'>
       <img alt='robo' src={`https://robohash.org/${id}?200*200`} />
       <div>
         <h2>{name}</h2>
