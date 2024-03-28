62c97f0198af78ac859d41eecfd395e52334a133
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-28 07:00:11 +0530

    this.props.value is undefined

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -3,8 +3,7 @@ import React, { Component } from "react";
 class AppFunction extends Component {
     constructor(props) {
       super(props);
-      console.log(props.value);
-      // this.clickFunction = this.clickFunction.bind(this);
+      this.clickFunction = this.clickFunction.bind(this);
     }  clickFunction() {
       console.log(this.props.value);
     }  render() {
