df51b1ddb417a044c9070069ac8a8c929f13a86e
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-28 07:29:51 +0530

    colors have been updated

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,7 +4,7 @@ import React from "react";
 function SearchBox({ searchChange }) {
     return(
         <div className="pa2 ma2">
-            <input onChange={searchChange} className="pa3 ba b-dark-green bg-dark-green" type="search" placeholder="Search robots"/>
+            <input onChange={searchChange} className="pa3 ba b-light-green bg-light-green" type="search" placeholder="Search robots"/>
         </div>
     )
 }
