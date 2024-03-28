7fc66fcb4aea76d1a23b0c5afc22d05f7c68b019
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-28 07:04:44 +0530

    Checking AppFunction

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
         <h1 className="pa5 br4 bg-dark-green">Robo Friends</h1>
         <SearchBox searchChange={onSearchChange} />
         <CardList  robots={filteredRobots} />
-        <AppFunction robots={filteredRobots}/>
+        <AppFunction value={filteredRobots}/>
     </div>
   );
 };
