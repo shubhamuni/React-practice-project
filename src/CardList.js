06c4044b932a6733854526ea55a466d3059f3791
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-28 08:32:51 +0530

    passed name of city in CardList

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,7 @@ import Card from "./Card";
 const CardList = ({ robot }) => {
     return(
         robot.map((user,i)=>{
-            return <Card key={i} name={robot[i].name} id={robot[i].id} email={robot[i].email} city={robot[i].city}/>;
+            return <Card key={i} name={robot[i].name} id={robot[i].id} email={robot[i].email} city={robot[i].address.city}/>;
         })    
     );
 }
