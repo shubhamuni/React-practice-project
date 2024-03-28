e2b6ef7fc599bc4e38050eca65cf484970d6898b
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-28 19:35:31 +0530

    saved and edited

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -2,16 +2,7 @@ import React from "react";
 
 
 function AppFunction () {
-  const obj = {
-    a:10,
-    x: () => {
-      const y = () =>{
-        console.log(this);
-      }
-      y();
-    },
-  };
-  obj.x();
+  
   return(
     <p>Checking code</p>
   )
