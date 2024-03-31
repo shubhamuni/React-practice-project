a8374c5529f0392b1fbffba4e028dd029643f73e
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-31 07:59:22 +0530

    check some css values

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -19,13 +19,12 @@ function AppFunction () {
   }
   fullName.call(object1,"Malegaon","From call of maharashtra")
   fullName.apply(object1,["Malegaon,","Maharashtra"])
-  const newLocal = <label for="business-account"><input for="business-account" id="business-account" type="radio" name="account-type" /> Business</label>;
   return(
     <div>
       <fieldset>
         <legend aria-required>Account type (required)</legend>
         <label for="personal-account"><input for="personal-account" id="personal-account" type="radio" name="account-type" checked /> Personal</label>
-        {newLocal}
+        <label for="business-account"><input for="business-account" id="business-account" type="checkbox" name="account-type" /> Business</label>
       </fieldset>
     </div>
   ) 
