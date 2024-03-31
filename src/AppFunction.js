4e90892b7388bffb55d38dbb8fc964ac319df47f
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-03-31 19:12:34 +0530

    reverted to previous version

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -21,13 +21,9 @@ function AppFunction () {
   fullName.apply(object1,["Malegaon,","Maharashtra"])
   return(
     <div>
-      <fieldset>
-        <legend aria-required>Account type (required)</legend>
-        <label for="personal-account"><input for="personal-account" id="personal-account" type="radio" name="account-type" checked /> Personal</label>
-        <label for="business-account"><input for="business-account" id="business-account" type="checkbox" name="account-type" /> Business</label>
-      </fieldset>
+      <p>Checking code</p>
     </div>
-  ) 
+  )
 }
 
 export default AppFunction;
