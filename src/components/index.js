3da084c254d0923691276b67a87fefa9c039018e
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-06 11:44:29 +0530

    clearButton

--- src/components/index.js
+++ src/components/index.js
@@ -97,4 +97,4 @@ function clearForm() {
 
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
-
+clearButton.addEventListener("click", clearForm)
