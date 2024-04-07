7db76e4d85c93e7f603e0ad61308b7dfa5eb36e6
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-07 09:50:11 +0530

    audio pause

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,22 @@ audio.addEventListener("ended", () => {
   const currentSongIndex = getCurrentSongIndex();
   const nextSongExists = currentSongIndex < userData.songs.length - 1;
 });
+
+const sortSongs = () => {
+  userData?.songs.sort((a,b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return userData?.songs;
+};
+
+renderSongs(sortSongs());
+setPlayButtonAccessibleText();
