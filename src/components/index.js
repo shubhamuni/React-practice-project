5c1889b9108850ec72105561dc6536e43f24fcbd
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-08 19:55:50 +0530

    split

--- src/components/index.js
+++ src/components/index.js
@@ -14,6 +14,6 @@ currentDateParagraph.textContent=formattedDate;
 dateOptionsSelectElement.addEventListener("change", () => {
   switch (dateOptionsSelectElement.value){
     default case "yyyy-mm-dd" :
-    currentDateParagraph.textContent = formattedDate.split("-").reverse();
+    currentDateParagraph.textContent = formattedDate.split("-").reverse().join("-");
   }
 });
