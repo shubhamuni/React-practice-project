61ed6c64c79ccbb41633a13126de74fe5f0b2176
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-08 19:49:26 +0530

    switch

--- src/components/index.js
+++ src/components/index.js
@@ -11,4 +11,6 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent=formattedDate;
 
-dateOptionsSelectElement.addEventListener("change", () => {});
+dateOptionsSelectElement.addEventListener("change", () => {
+  switch (dateOptionsSelectElement.value){}
+});
