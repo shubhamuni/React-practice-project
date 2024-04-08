a22457cd97e0a5af53a2d9d46afa9b6a5086fbfc
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-08 19:59:28 +0530

    template literal

--- src/components/index.js
+++ src/components/index.js
@@ -17,7 +17,7 @@ dateOptionsSelectElement.addEventListener("change", () => {
     currentDateParagraph.textContent = formattedDate.split("-").reverse().join("-")
     break;
     case "mm-dd-yyyy-h-mm":
-      currentDateParagraph.textContent = ``
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
       break;
   }
 });
