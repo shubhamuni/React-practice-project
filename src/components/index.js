69e282e183506d97a9a61b4d146808a7c0f470ac
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-10 20:49:00 +0530

    pusing to main

--- src/components/index.js
+++ src/components/index.js
@@ -1,201 +1,110 @@
-const teamName = document.getElementById("team");
-const typeOfSport = document.getElementById("sport");
-const worldCupYear = document.getElementById("year");
-const headCoach = document.getElementById("head-coach");
-const playerCards = document.getElementById("player-cards");
-const playersDropdownList = document.getElementById("players");
-const myFavoriteFootballTeam = {
-  team: "Argentina",
-  sport: "Football",
-  year: 1986,
-  isWorldCupWinner: true,
-  headCoach: {
-    coachName: "Carlos Bilardo",
-    matches: 7,
-  },
-  players: [
-    {
-      name: "Sergio Almirón",
-      position: "forward",
-      number: 1,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "Sergio Batista",
-      position: "midfielder",
-      number: 2,
-      isCaptain: false,
-      nickname: null,
-    },
-     {
-      name: "Ricardo Bochini",
-      position: "midfielder",
-      number: 3,
-      isCaptain: false,
-      nickname: "El Bocha",
-    },
-    {
-      name: "Claudio Borghi",
-      position: "midfielder",
-      number: 4,
-      isCaptain: false,
-      nickname: "Bichi",
-    },
-    {
-      name: "José Luis Brown",
-      position: "defender",
-      number: 5,
-      isCaptain: false,
-      nickname: "Tata",
-    },
-    {
-      name: "Daniel Passarella",
-      position: "defender",
-      number: 6,
-      isCaptain: false,
-      nickname: "El Gran Capitán",
-    },
-    {
-      name: "Jorge Burruchaga",
-      position: "forward",
-      number: 7,
-      isCaptain: false,
-      nickname: "Burru",
-    },
-    {
-      name: "Néstor Clausen",
-      position: "defender",
-      number: 8,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "José Luis Cuciuffo",
-      position: "defender",
-      number: 9,
-      isCaptain: false,
-      nickname: "El Cuchu",
-    },
-    {
-      name: "Diego Maradona",
-      position: "midfielder",
-      number: 10,
-      isCaptain: true,
-      nickname: "El Pibe de Oro",
-    },
-    {
-      name: "Jorge Valdano",
-      position: "forward",
-      number: 11,
-      isCaptain: false,
-      nickname: "The Philosopher of Football",
-    },
-    {
-      name: "Héctor Enrique",
-      position: "midfielder",
-      number: 12,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "Oscar Garré",
-      position: "defender",
-      number: 13,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "Ricardo Giusti",
-      position: "midfielder",
-      number: 14,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "Luis Islas",
-      position: "goalkeeper",
-      number: 15,
-      isCaptain: false,
-      nickname: "El loco",
-    },
-    {
-      name: "Julio Olarticoechea",
-      position: "defender",
-      number: 16,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "Pedro Pasculli",
-      position: "forward",
-      number: 17,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "Nery Pumpido",
-      position: "goalkeeper",
-      number: 18,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "Oscar Ruggeri",
-      position: "defender",
-      number: 19,
-      isCaptain: false,
-      nickname: "El Cabezón",
-    },
-    {
-      name: "Carlos Tapia",
-      position: "midfielder",
-      number: 20,
-      isCaptain: false,
-      nickname: null,
-    },
-    {
-      name: "Marcelo Trobbiani",
-      position: "midfielder",
-      number: 21,
-      isCaptain: false,
-      nickname: "Calesita",
-    },
-    {
-      name: "Héctor Zelada",
-      position: "goalkeeper",
-      number: 22,
-      isCaptain: false,
-      nickname: null,
-    },
-  ],
-};
-Object.freeze(myFavoriteFootballTeam);
-const { coachName } = myFavoriteFootballTeam.headCoach;
-const { sport, team, year, players } = myFavoriteFootballTeam;
-typeOfSport.textContent = sport;
-teamName.textContent = team;
-worldCupYear.textContent = year;
-headCoach.textContent = coachName;
-const setPlayerCards = (arr= players) => {};
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
 
-playerCards.innerHTML += arr.map(({ name, position, number, isCaptain, nickname })=>{
-  <div class="player-card">
-    <h2>${name}${isCaptain ? "(Captain)" : ""}</h2>
-    <p>Position: ${position}</p>
-    <p>Number: ${number}</p>
-    <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
-  </div>
-}).join("");
-//To remove the commas between each player-card so it does not show up on screen, chain the .join() method to the .map() method. Pass an empty string as the argument for the .join() method.
+const taskData = [];
+let currentTask = {};
 
-//For the callback function, pass in e as a parameter.e represents an object which contains the information for that event.
+const addOrUpdateTask = () => {
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
 
-
-playersDropdownList.addEventListener('change', (e) => {
-  playerCards.innerHTML = "";
-  switch (e.target.value) {
-    case "nickname":
-      setPlayerCards(players.filter(player => player.nickname !== null));
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
   }
+
+
+  updateTaskContainer()
+  reset()
+};
+
+const updateTaskContainer = () => {
+  tasksContainer.innerHTML = "";
+
+  taskData.forEach(
+    ({ id, title, date, description }) => {
+        (tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+          <button onclick="deleteTask(this)" type="button" class="btn">Delete</button> 
+        </div>
+      `)
+    }
+  );
+};
+
+
+const deleteTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+}
+
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+
+  currentTask = taskData[dataArrIndex];
+
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;
+
+  addOrUpdateTaskBtn.innerText = "Update Task";
+
+  taskForm.classList.toggle("hidden");  
+}
+
+const reset = () => {
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+}
+
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
+
+closeTaskFormBtn.addEventListener("click", () => {
+  const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  if (formInputsContainValues) {
+    confirmCloseDialog.showModal();
+  } else {
+    reset();
+  }
+});
+
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  reset()
+});
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addOrUpdateTask();
 });
