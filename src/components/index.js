e13e78f8c9574c3ad34fa8348c303659475aad78
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-10 08:23:21 +0530

    parameters passed

--- src/components/index.js
+++ src/components/index.js
@@ -182,5 +182,8 @@ const setPlayerCards = (arr= players) => {};
 playerCards.innerHTML += arr.map(({ name, position, number, isCaptain, nickname })=>{
   <div class="player-card">
     <h2>${name}${isCaptain ? "(Captain)" : ""}</h2>
+    <p>Position: ${position}</p>
+    <p>Number: ${number}</p>
+    <p>Nickname: </p>
   </div>
 });
