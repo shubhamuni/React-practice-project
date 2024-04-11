7a52e822d994bc3d8869f8b1b97d205f5447bca3
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-11 12:59:30 +0530

    decimal to Binary function is complete

--- src/components/index.js
+++ src/components/index.js
@@ -5,14 +5,29 @@ const result = document.getElementById("result");
 const decimalToBinary = (input) => {
   const inputs = [];
   const quotients = [];
-  const remainders = []
+  const remainders = [];
+
+  if (input === 0) {
+    result.innerText = "0";
+    return;
+  }
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
+
+  result.innerText = remainders.reverse().join("");
 };
-while(input > 0){
-  input = 0;
-  const quotient =Math.floor(input / 2);
-}
-
-
 
 const checkUserInput = () => {
   if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
