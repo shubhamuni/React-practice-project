3b75db9a9780729864993bf5c304091e741b5d78
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-12 11:16:11 +0530

    timeout

--- src/components/index.js
+++ src/components/index.js
@@ -28,17 +28,7 @@ const decimalToBinary = (input) => {
   }
 };
 
-const showAnimation = () => {
-  setTimeout(() => {
-    console.log("free");
-  }, 500);
-  setTimeout(() => {
-    console.log("Code");
-  }, 1000);
-  setTimeout(() => {
-    console.log("Camp");
-  }, 1500);
-};
+
 
 const checkUserInput = () => {
   const inputInt = parseInt(numberInput.value);
