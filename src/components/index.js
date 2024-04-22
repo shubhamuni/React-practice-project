4a015eb97d8af12bd96345a21c1fe93da074d00a
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-22 11:45:30 +0530

    rows array

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,3 @@
 let character = 'Hello';
-console.log(character);
-character = "World";
-let secondCharacter;
-secondCharacter = character;
-console.log(secondCharacter);
 let count = 8;
-console.log(count + 1)
+let rows = []
