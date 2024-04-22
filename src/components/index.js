ca239d552eeeb6e714cd56e66fdfb26865ade555
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-22 11:17:23 +0530

    taskObj

--- src/components/index.js
+++ src/components/index.js
@@ -1,79 +1,40 @@
-const numberInput = document.getElementById("number-input");
-const convertBtn = document.getElementById("convert-btn");
-const result = document.getElementById("result");
-const animationContainer = document.getElementById("animation-container");
-const animationData = [
-  {
-    inputVal: 5,
-    marginTop: 300,
-    addElDelay: 1000,
-    msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
-    showMsgDelay: 15000,
-    removeElDelay: 20000,
-  },
-  {
-    inputVal: 2,
-    marginTop: -200,
-    addElDelay: 1500,
-    msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
-    showMsgDelay: 10000,
-    removeElDelay: 15000,
-  },
-  {
-    inputVal: 1,
-    marginTop: -200,
-    addElDelay: 2000,
-    msg: 'decimalToBinary(1) returns "1" (base case) and gives that value to the stack below. Then it pops off the stack.',
-    showMsgDelay: 5000,
-    removeElDelay: 10000,
-  }
-];
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
+const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
+const cancelBtn = document.getElementById("cancel-btn");
+const discardBtn = document.getElementById("discard-btn");
+const tasksContainer = document.getElementById("tasks-container");
+const titleInput = document.getElementById("title-input");
+const dateInput = document.getElementById("date-input");
+const descriptionInput = document.getElementById("description-input");
 
-const decimalToBinary = (input) => {
-  if (input === 0 || input === 1) {
-    return String(input);
-  } else {
-    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
-  }
-};
+const taskData = [];
+let currentTask = {};
 
-const showAnimation = () => {
-  result.innerText = "Call Stack Animation";
+openTaskFormBtn.addEventListener("click", () =>
+  taskForm.classList.toggle("hidden")
+);
 
-  animationData.forEach((obj) => {
-    setTimeout(() => {
-      animationContainer.innerHTML += `
-        <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px;" class="animation-frame">
-          decimalToBinary(${obj.inputVal})
-        </p>
-      `;
-    }, obj.addElDelay);
+closeTaskFormBtn.addEventListener("click", () => {
+  confirmCloseDialog.showModal();
+});
 
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 
-  });
-};
+discardBtn.addEventListener("click", () => {
+  confirmCloseDialog.close();
+  taskForm.classList.toggle("hidden");
+});
 
-const checkUserInput = () => {
-  const inputInt = parseInt(numberInput.value);
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  if (!numberInput.value || isNaN(inputInt)) {
-    alert("Please provide a decimal number");
-    return;
-  }
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
 
-  if (inputInt === 5) {
-    showAnimation();
-    return;
-  }
-
-  result.textContent = decimalToBinary(inputInt);
-  numberInput.value = "";
-};
-
-convertBtn.addEventListener("click", checkUserInput);
-
-numberInput.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    checkUserInput();
-  }
+  const taskObj = {
+    id: titleInput.value.toLowerCase().split(" ")
+  };
+  console.log(taskObj);
 });
