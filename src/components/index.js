d5354e1f942be2389e1e01da9b0d25f73c2c4bec
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-23 11:40:31 +0530

    padRow updated

--- src/components/index.js
+++ src/components/index.js
@@ -2,12 +2,13 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow() {
+function padRow(rowNumber, rowCount) {
+    return character.repeat(rowNumber)
   }
 
 
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1))
+  rows.push(padRow())
 }
 
 let result = ""
