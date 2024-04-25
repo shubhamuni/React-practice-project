b436fe8ea6ba795db719b1358bf0ae363dbc6c5b
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-25 19:52:35 +0530

    used ternary operator for currentProductCountSpan

--- src/components/index.js
+++ src/components/index.js
@@ -120,4 +120,5 @@ class ShoppingCart {
     const currentProductCount = totalCountPerProduct[product.id];
     const currentProductCountSpan = document.getElementById(`product-count-for-id${product.id}`)
   }
+  currentProductCount > 1 ? currentProductCountSpan.textContent = `${currentProductCount}x` : undefined; 
 };
