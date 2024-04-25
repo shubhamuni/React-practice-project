2f8a11f933783ca7f9f005211a3ce610227102de
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-25 15:40:38 +0530

    push

--- src/components/index.js
+++ src/components/index.js
@@ -92,7 +92,16 @@ products.forEach(
         <h2>${name}</h2>
         <p class="dessert-price">$${price}</p>
         <p class="product-category">Category: ${category}</p>
+        <button 
+          id="${id}" 
+          class="btn add-to-cart-btn">Add to cart
+        </button>
       </div>
     `;
   }
 );
+
+class ShoppingCart {
+            
+}
+        const myShoppingCart = new ShoppingCart();
