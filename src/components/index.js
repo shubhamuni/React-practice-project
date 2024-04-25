67fe22b21d1177b7d518bb1a628164ba3c79ed81
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-25 07:45:27 +0530

    updating code

--- src/components/index.js
+++ src/components/index.js
@@ -84,3 +84,15 @@ const products = [
     category: "Cupcake",
   },
 ];
+
+products.forEach(
+  ({ name, id, price, category }) => {
+    dessertCards.innerHTML += `
+      <div class="dessert-card">
+        <h2>${name}</h2>
+        <p class="dessert-price">$ ${price}</p>
+        <p class="product-category">Category: ${category}</p>
+      </div>
+    `;
+  }
+);
