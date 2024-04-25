7318f3eabab0a8775693c2087a349c5801f68bef
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-25 19:57:19 +0530

    undefined changed to addition of div into productscontainer.innerHTML

--- src/components/index.js
+++ src/components/index.js
@@ -120,5 +120,5 @@ class ShoppingCart {
     const currentProductCount = totalCountPerProduct[product.id];
     const currentProductCountSpan = document.getElementById(`product-count-for-id${product.id}`)
   }
-  currentProductCount > 1 ? currentProductCountSpan.textContent = `${currentProductCount}x` : undefined; 
+  currentProductCount > 1 ? currentProductCountSpan.textContent = `${currentProductCount}x` : productsContainer.innerHTML+=`<div class="product" id="dessert${id}"></div>`; 
 };
