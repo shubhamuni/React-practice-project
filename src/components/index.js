03522c2f5c7b8cd76d07c82494c651605813c888
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-26 18:37:54 +0530

    this.width = 200;

--- src/components/index.js
+++ src/components/index.js
@@ -56,8 +56,20 @@ class Player {
   }
 }
 
-const player = new Player();
 
+class Platform {
+  constructor(x, y) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = 200;
+
+  }
+}
+
+
+const player = new Player();
 
 const animate = () => {
   requestAnimationFrame(animate);
@@ -90,7 +102,6 @@ const movePlayer = (key, xVelocity, isPressed) => {
     return;
   }
 
-
   switch (key) {
     case "ArrowLeft":
       keys.leftKey.pressed = isPressed;
@@ -104,16 +115,27 @@ const movePlayer = (key, xVelocity, isPressed) => {
     case "Spacebar":
       player.velocity.y -= 8;
       break;
-    
+    case "ArrowRight":
+      keys.rightKey.pressed = isPressed;
+      if (xVelocity === 0) {
+        player.velocity.x = xVelocity;
+      }
+      player.velocity.x += xVelocity;
   }
-
 }
-
 
 const startGame = () => {
   canvas.style.display = "block";
   startScreen.style.display = "none";
-  player.draw();
+  animate();
 }
 
 startBtn.addEventListener("click", startGame);
+
+window.addEventListener("keydown", ({ key }) => {
+  movePlayer(key, 8, true);
+});
+
+window.addEventListener("keyup", ({ key }) => {
+  movePlayer(key, 0, false);
+});
