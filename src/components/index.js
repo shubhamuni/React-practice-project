d7a54edabef32b43efcc04464212d58ec077ed9e
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-27 18:35:26 +0530

    bubbleSort()

--- src/components/index.js
+++ src/components/index.js
@@ -6,12 +6,29 @@ const sortInputArray = (event) => {
   const inputValues = [
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
+
+  const sortedValues = bubbleSort(inputValues);
+
+  updateUI(sortedValues);
 }
 
 const updateUI = (array = []) => {
   array.forEach((num, i) => {
-
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
   })
+}
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      console.log(array, array[j], array[j + 1]);
+
+      if (array[j] > array[j + 1]) {
+
+      }
+    }
+  }
 }
 
 sortButton.addEventListener("click", sortInputArray);
