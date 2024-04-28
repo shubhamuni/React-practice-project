46e6c17d2eab198428fc8a5d4a949029b561c1fc
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-28 07:26:34 +0530

    getMedian()

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,15 @@ const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length
 
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
 }
+
+
+
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -10,6 +18,7 @@ const calculate = () => {
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
   
   const mean = getMean(numbers);
+  const median = getMedian(numbers);
 
   document.querySelector("#mean").textContent = mean;
-}
+  document.querySelector("#median").textContent = median;
