9956d90eaf3c098f30d7384a2d8b5a4526f9cea2
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-28 16:16:12 +0530

    completing highPrecedence() function

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,13 @@ const infixToFunction = {
   "/": (x, y) => x / y,
 }
 
-const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => {});
+const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator](parseFloat(arg1), parseFloat(arg2)));
+
+const highPrecedence = str => {
+  const regex = /([\d.]+)([*\/])([\d.]+)/;
+  const str2 = infixEval(str, regex);
+  return str === str2 ? str : highPrecedence(str2);
+}
 
 const isEven = num => num % 2 === 0;
 const sum = nums => nums.reduce((acc, el) => acc + el, 0);
@@ -25,6 +31,8 @@ const spreadsheetFunctions = {
   average,
   median
 }
+
+
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
