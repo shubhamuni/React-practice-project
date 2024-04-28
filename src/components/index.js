a5abca30ed2c5c681c6a9ddd3b84b499a9391786
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-28 06:15:11 +0530

    statistics calculator

--- src/components/index.js
+++ src/components/index.js
@@ -1,55 +1,15 @@
-const sortButton = document.getElementById("sort");
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
-const sortInputArray = (event) => {
-  event.preventDefault();
-
-  const inputValues = [
-    ...document.getElementsByClassName("values-dropdown")
-  ].map((dropdown) => Number(dropdown.value));
-
-  const sortedValues = selectionSort(inputValues);
-
-  updateUI(sortedValues);
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
 }
 
-const updateUI = (array = []) => {
-  array.forEach((num, i) => {
-    const outputValueNode = document.getElementById(`output-value-${i}`);
-    outputValueNode.innerText = num;
-  })
+const calculate = () => {
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  
+  const mean = getMean(numbers);
+
+  document.querySelector("#mean").textContent = mean;
 }
-
-const bubbleSort = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        const temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-      }
-    }
-  }
-
-  return array;
-}
-
-const selectionSort = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    let minIndex = i;
-
-    for (let j = i + 1; j < array.length; j++) {
-      console.log(array, array[j], array[minIndex]);
-      if (array[j] < array[minIndex]) {
-        minIndex = j;
-      }
-    }
-
-    const temp = array[i];
-    array[i] = array[minIndex];
-    array[minIndex] = temp;
-  }
-
-  return array;
-}
-
-sortButton.addEventListener("click", sortInputArray);
