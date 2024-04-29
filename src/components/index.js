467ea32ea5ec3c02b757818ef5f4b5a4d8acbd86
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-29 14:53:55 +0530

    avatar url

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
-const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
