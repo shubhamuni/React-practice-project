c732295e1e2b9def0631ab8f7c14d00a6cc5d9c1
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-29 07:04:34 +0530

    rolls are updating

--- src/components/index.js
+++ src/components/index.js
@@ -16,7 +16,34 @@ let score = 0;
 let totalScore = 0;
 let round = 1; 
 let rolls = 0; 
-  const rollDice = () => {}
+
+const rollDice = () => {
+  diceValuesArr = [];
+
+  for (let i = 0; i < 5; i++) {
+    const randomDice = Math.floor(Math.random() * 6) + 1;
+    diceValuesArr.push(randomDice);
+  };
+
+  listOfAllDice.forEach((dice, index) => {
+    dice.textContent = diceValuesArr[index];
+  });
+};
+
+const updateStats = () => {
+  currentRoundRollsText.textContent = rolls;
+  currentRoundText.textContent = round;
+};
+
+rollDiceBtn.addEventListener("click", () => {
+  if (rolls === 3) {
+    alert("You have made three rolls this round. Please select a score.");
+  } else {
+    rolls++;
+    rollDice();
+
+  }
+});
 
 rulesBtn.addEventListener("click", () => {
   isModalShowing = !isModalShowing;
