8a88e451d1974a87916499007e67e727890e3b2e
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-29 19:28:30 +0530

    avatar url

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
-
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const postsContainer = document.getElementById("posts-container");
 
 const allCategories = {
