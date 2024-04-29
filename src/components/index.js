f82df46221100356673c7f93b362e0440e88dc50
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-29 06:33:19 +0530

    all elements selected

--- src/components/index.js
+++ src/components/index.js
@@ -5,4 +5,7 @@ const currentRoundText = document.getElementById("current-round");
 const currentRoundRollsText = document.getElementById("current-round-rolls");
 const totalScoreText = document.getElementById("total-score");
 const scoreHistory = document.getElementById("score-history");
-    
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesContainer = document.querySelector(".rules-container");
+const rulesBtn = document.getElementById("rules-btn");
