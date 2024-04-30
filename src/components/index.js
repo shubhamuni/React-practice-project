5252a7295f4f4495930160430f3cc924071ab0c8
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-04-30 14:21:17 +0530

    intigrating with jQuery

--- src/components/index.js
+++ src/components/index.js
@@ -112,12 +112,13 @@ const showLatestPosts = (data) => {
     </tr>`;
   }).join("");
 };
-$(document).ready(function() {
-  $("#target1").css("color", "red");
-  $("#target1").prop("disabled", true);
-  $("#target4").remove();
-  $("#target2").appendTo("#right-well");
-  $("#target5").clone().appendTo("#left-well");
-  $("#target1").parent().css("background-color", "red");
+  $(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+    $("#target5").clone().appendTo("#left-well");
+    $("#target1").parent().css("background-color", "red");
+    $("#right-well").children().css("color", "orange");
 
-});
+  });
