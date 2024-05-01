1bad353a8cfde3f0fbfb60d7895691e11cc5d423
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-01 16:24:49 +0530

    creating a controller input

--- src/components/index.js
+++ src/components/index.js
@@ -1,44 +1,25 @@
-class Counter extends React.Component {
+class ControlledInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      input: ''
     };
-    // Bind the methods to the class
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.reset = this.reset.bind(this);
-  }
+    // Change code below this line
 
-  // Method to increment count
-  increment() {
-    this.setState(prevState => ({
-      count: prevState.count + 1
-    }));
+    // Change code above this line
   }
+  // Change code below this line
 
-  // Method to decrement count
-  decrement() {
-    this.setState(prevState => ({
-      count: prevState.count - 1
-    }));
-  }
-
-  // Method to reset count to 0
-  reset() {
-    this.setState({
-      count: 0
-    });
-  }
-
+  // Change code above this line
   render() {
     return (
       <div>
-        <button className='inc' onClick={this.increment}>Increment!</button>
-        <button className='dec' onClick={this.decrement}>Decrement!</button>
-        <button className='reset' onClick={this.reset}>Reset</button>
-        <h1>Current Count: {this.state.count}</h1>
+        { /* Change code below this line */}
+
+        { /* Change code above this line */}
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
       </div>
     );
   }
-}
+};
