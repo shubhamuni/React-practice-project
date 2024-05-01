74981253631505567d352e52eb4148f5039887aa
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-01 05:59:01 +0530

    react code (JSX)

--- src/components/index.js
+++ src/components/index.js
@@ -1,127 +1,19 @@
-const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
-const forumTopicUrl = "https://forum.freecodecamp.org/t/";
-const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
-const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
-const postsContainer = document.getElementById("posts-container");
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+}
 
-const allCategories = {
-  299: { category: "Career Advice", className: "career" },
-  409: { category: "Project Feedback", className: "feedback" },
-  417: { category: "freeCodeCamp Support", className: "support" },
-  421: { category: "JavaScript", className: "javascript" },
-  423: { category: "HTML - CSS", className: "html-css" },
-  424: { category: "Python", className: "python" },
-  432: { category: "You Can Do This!", className: "motivation" },
-  560: { category: "Backend Development", className: "backend" },
-};
+Items.defaultProps = {
+  quantity: 0
+}
 
-const forumCategory = (id) => {
-  let selectedCategory = {};
-
-  if (allCategories.hasOwnProperty(id)) {
-    const { className, category } = allCategories[id];
-
-    selectedCategory.className = className;
-    selectedCategory.category = category;
-  } else {
-    selectedCategory.className = "general";
-    selectedCategory.category = "General";
-    selectedCategory.id = 1;
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
   }
-  const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
-  const linkText = selectedCategory.category;
-  const linkClass = `category ${selectedCategory.className}`;
-
-  return `<a href="${url}" class="${linkClass}" target="_blank">
-    ${linkText}
-  </a>`;
-};
-
-const timeAgo = (time) => {
-  const currentTime = new Date();
-  const lastPost = new Date(time);
-
-  const timeDifference = currentTime - lastPost;
-  const msPerMinute = 1000 * 60;
-
-  const minutesAgo = Math.floor(timeDifference / msPerMinute);
-  const hoursAgo = Math.floor(minutesAgo / 60);
-  const daysAgo = Math.floor(hoursAgo / 24);
-
-  if (minutesAgo < 60) {
-    return `${minutesAgo}m ago`;
-  }
-
-  if (hoursAgo < 24) {
-    return `${hoursAgo}h ago`;
-  }
-
-  return `${daysAgo}d ago`;
-};
-
-const viewCount = (views) => {
-  const thousands = Math.floor(views / 1000);
-
-  if (views >= 1000) {
-    return `${thousands}k`;
-  }
-
-  return views;
-};
-
-
-
-const fetchData = async () => {
-  try {
-    const res = await fetch(forumLatest);
-    const data = await res.json();
-    showLatestPosts(data);
-  } catch (err) {
-    console.log(err);
+  render() {
+    { /* Change code below this line */ }
+    return <Items quantity={10}/>
+    { /* Change code above this line */ }
   }
 };
-
-fetchData();
-
-const showLatestPosts = (data) => {
-  const { topic_list, users } = data;
-  const { topics } = topic_list;
-
-  postsContainer.innerHTML = topics.map((item) => {
-    const {
-      id,
-      title,
-      views,
-      posts_count,
-      slug,
-      posters,
-      category_id,
-      bumped_at,
-    } = item;
-
-    return `
-    <tr>
-      <td>
-        <p class="post-title">${title}</p>
-        ${forumCategory(category_id)}
-      </td>
-      <td></td>
-      <td>${posts_count - 1}</td>
-      <td>${viewCount(views)}</td>
-      <td>${timeAgo(bumped_at)}</td>
-    </tr>`;
-  }).join("");
-};
-$(document).ready(function() {
-  $("#target1").css("color", "red");
-  $("#target1").prop("disabled", true);
-  $("#target4").remove();
-  $("#target2").appendTo("#right-well");
-  $("#target5").clone().appendTo("#left-well");
-  $("#target1").parent().css("background-color", "red");
-  $("#right-well").children().css("color", "orange");
-  $("#left-well").children().css("color", "green");
-  $(".target:nth-child(2)").addClass("animated bounce");
-  $(".target:even").addClass("animated shake");
-  $("body").addClass("animated hinge");
-});
+Items.defaultProps = { quantity: 0 }
