0c967f776408286880a800b0e3f8fe6d93e38267
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-01 17:16:13 +0530

    Use the Lifecycle Method componentDidMount

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-
 class MyComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      activeUsers: null
+    };
   }
-
-  componentWillMount() {
-    console.log('Component is about to be mounted to the DOM.');
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 2500);
   }
-
   render() {
-    return <div />;
+    return (
+      <div>
+        {/* Change code below this line */}
+        <h1>Active Users: {this.state.activeUsers}</h1>
+        {/* Change code above this line */}
+      </div>
+    );
   }
 }
-
-export default MyComponent;
