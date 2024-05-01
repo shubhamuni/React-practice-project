68af02c777237d0249545f2de44231c3c2690247
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-01 08:00:55 +0530

    Set State with this.setState

--- src/components/index.js
+++ src/components/index.js
@@ -2,18 +2,22 @@ class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'freeCodeCamp'
-    }
+      name: 'Initial State'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    // Change code below this line
+    this.setState({
+      name: 'React Rocks!'
+    });
+    // Change code above this line
   }
   render() {
-    // Change code below this line
-    const name = this.state.name;
-    // Change code above this line
     return (
       <div>
-        { /* Change code below this line */ }
-        <h1>{name}</h1>
-        { /* Change code above this line */ }
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
       </div>
     );
   }
