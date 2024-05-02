48abbed3024f2bbb1833c3106326519f408f3be3
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-02 19:08:27 +0530

    reducer

--- src/components/index.js
+++ src/components/index.js
@@ -12,7 +12,7 @@ const immutableReducer = (state = todos, action) => {
   switch(action.type) {
     case ADD_TO_DO:
       // Don't mutate state here or the tests will fail
-      return [...state, action.todo]
+      return [...state]
     default:
       return state;
   }
