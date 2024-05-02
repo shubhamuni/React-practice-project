4c0f824ac428aaff6f4757be9cb1ab43bcc5cb3f
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-02 07:22:03 +0530

    react redux chellenge

--- src/components/index.js
+++ src/components/index.js
@@ -1,40 +1,25 @@
-import React from 'react';
+const defaultState = {
+  login: false
+};
 
-class GateKeeper extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ input: event.target.value });
-  }
-
-  render() {
-    let inputStyle = {
-      border: '1px solid black'
-    };
-
-    // Conditionally update inputStyle based on input length
-    if (this.state.input.length > 15) {
-      inputStyle.border = '3px solid red';
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+  switch (action.type) {
+    case "LOGIN" :
+    return {
+    ...state,
+    login: true
+  };
+    default:
+    return state;
     }
+  // Change code above this line
+};
 
-    return (
-      <div>
-        <h3>Don't Type Too Much:</h3>
-        <input
-          type="text"
-          style={inputStyle}
-          value={this.state.input}
-          onChange={this.handleChange}
-        />
-      </div>
-    );
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
   }
-}
-
-export default GateKeeper;
+};
