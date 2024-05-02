743626373d6c5bf8e8a482b28537c049c9a652ac
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-02 08:29:14 +0530

    send action data to store solved

--- src/components/index.js
+++ src/components/index.js
@@ -1,37 +1,27 @@
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
+const ADD_NOTE = 'ADD_NOTE';
 
-const counterReducer = (state = 0, action) => {
+const notesReducer = (state = 'Initial State', action) => {
   switch(action.type) {
-    case INCREMENT:
-      return state + 1;
-    case DECREMENT:
-      return state - 1;
+    // Change code below this line
+    case ADD_NOTE :
+      return action.text;
+    // Change code above this line
     default:
       return state;
   }
 };
 
-const LOGIN = 'LOGIN';
-const LOGOUT = 'LOGOUT';
-
-const authReducer = (state = {authenticated: false}, action) => {
-  switch(action.type) {
-    case LOGIN:
-      return {
-        authenticated: true
-      }
-    case LOGOUT:
-      return {
-        authenticated: false
-      }
-    default:
-      return state;
-  }
+const addNoteText = (note) => {
+  // Change code below this line
+  return {
+    type: ADD_NOTE,
+    text: note
+  };
+  // Change code above this line
 };
-const rootReducer = Redux.combineReducers({
-  count: counterReducer,
-  auth: authReducer
-});// Define the root reducer here
 
-const store = Redux.createStore(rootReducer);
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState());
