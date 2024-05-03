d12c66882f8cc6ad138a2dbe5c0987b647ab394a
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-03 07:36:16 +0530

    redux with connect method

--- src/components/index.js
+++ src/components/index.js
@@ -5,12 +5,29 @@ const addMessage = (message) => {
   }
 };
 
-// Change code below this line
+const mapStateToProps = (state) => {
+  return {
+    messages: state
+  }
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
     submitNewMessage: (message) => {
-      dispatch(addMessage(message))
+      dispatch(addMessage(message));
     }
   }
-}
+};
+
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h3>This is a Presentational Component</h3>
+  }
+};
+
+const connect = ReactRedux.connect;
+// Change code below this line
+const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(Presentational)
