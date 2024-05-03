aa6019c6aedcdb91d62287102aae8c9d2311d8e5
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-03 06:21:08 +0530

    mapStateToProps

--- src/components/index.js
+++ src/components/index.js
@@ -1,36 +1,8 @@
-class DisplayMessages extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        input: '',
-        messages: []
-      }
-    }
-    // Add handleChange() and submitMessage() methods here
-    handleChange = (event) => {
-      this.setState({ input: event.target.value });
-    }
-    submitMessage = () => {
-      this.setState({
-        messages: [...this.state.messages, this.state.input],
-        input: ''
-      });
-    }
-  
-    render() {
-      return (
-        <div>
-          <h2>Type in a new Message:</h2>
-          { /* Render an input, button, and ul below this line */ }
-          <input type='text' value={this.state.input} onChange={this.handleChange.bind(this)}/>
-          <button onClick={this.submitMessage}>Submit</button>
-          <ul>
-            {this.state.messages.map((message, index) => (
-              <li key={index}>{message}</li>
-            ))}
-          </ul>
-          { /* Change code above this line */ }
-        </div>
-      );
-    }
-  };
+const state = [];
+
+// Change code below this line
+const mapStateToProps = (state) => {
+  return{
+    messages: state
+  }
+} 
