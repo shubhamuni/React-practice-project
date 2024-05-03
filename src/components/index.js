3cab1f64899a9201d3d0fa8e56b3d58250b6a684
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-03 13:31:06 +0530

    some files changed

--- src/components/index.js
+++ src/components/index.js
@@ -1,94 +1,14 @@
-// Redux:
-const ADD = 'ADD';
+const editor = document.getElementById('editor');
+const preview = document.getElementById('preview');
 
-const addMessage = (message) => {
-  return {
-    type: ADD,
-    message: message
-  }
-};
+// Update preview on input
+editor.addEventListener('input', updatePreview);
 
-const messageReducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [
-        ...state,
-        action.message
-      ];
-    default:
-      return state;
-  }
-};
+// Initial rendering
+updatePreview();
 
-const store = Redux.createStore(messageReducer);
-
-// React:
-const Provider = ReactRedux.Provider;
-const connect = ReactRedux.connect;
-
-// Change code below this line
-class Presentational extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.submitMessage = this.submitMessage.bind(this);
-  }
-  handleChange(event) {
-    this.setState({
-      input: event.target.value
-    });
-  }
-  submitMessage() {
-    this.props.submitNewMessage(this.state.input);
-    this.setState({
-      input: ''
-    });
-  }
-  render() {
-    return (
-      <div>
-        <h2>Type in a new Message:</h2>
-        <input
-          value={this.state.input}
-          onChange={this.handleChange}/><br/>
-        <button onClick={this.submitMessage}>Submit</button>
-        <ul>
-          {this.props.messages.map( (message, idx) => {
-              return (
-                 <li key={idx}>{message}</li>
-              )
-            })
-          }
-        </ul>
-      </div>
-    );
-  }
-};
-// Change code above this line
-
-const mapStateToProps = (state) => {
-  return {messages: state}
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    submitNewMessage: (message) => {
-      dispatch(addMessage(message))
-    }
-  }
-};
-
-const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational);
-
-class AppWrapper extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Container/>
-      </Provider>
-    );
-  }
-};
+function updatePreview() {
+  const markdown = editor.value;
+  const html = marked(markdown);
+  preview.innerHTML = html;
+}
