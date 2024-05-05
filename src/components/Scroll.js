10225b1974bc530ea429062e754d220873489b9f
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-05 11:18:43 +0530

    scroll updated

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,6 @@ import React from "react";
 const Scroll = (props) => {
     return (
         <div style={{overflowY:"scroll",border:"2px solid black", height:"600px"}}>
-            <p>CardList component inside a scroll component</p>
             {props.children}
         </div>
     )
