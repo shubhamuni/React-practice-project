77eff272ee6d4710bc75746d19bbe22203283f8c
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-05 07:41:21 +0530

    code add

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,3 @@
-const editor = document.getElementById('editor');
-const preview = document.getElementById('preview');
-
-// Update preview on input
-editor.addEventListener('input', updatePreview);
-
-// Initial rendering
-updatePreview();
-
-function updatePreview() {
-  const markdown = editor.value;
-  const html = (markdown); 
-  preview.innerHTML = html;
-}
 
 // Below code is for printing count to the DOM where we click
 
