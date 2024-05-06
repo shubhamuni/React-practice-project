f5355c944e4f2891ab5c21e5e8ee20b4e531a2e7
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-06 19:50:39 +0530

    hasError: true

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -4,7 +4,7 @@ class ErrorBoundry extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            hasError: false
+            hasError: true
         }
     }
     //Error boundry for any error in CardList component
