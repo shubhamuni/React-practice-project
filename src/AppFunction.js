49ee8d9e2594ffeaedea4a9a3856ca32a4d301bb
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-07 08:07:27 +0530

    error handling checked

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -4,17 +4,14 @@ import Freecodecamp from "./components/Freecodecamp";
 
 function AppFunction () {
   const promise = new Promise((resolve, reject) => {
-    if(true) {
+    if(false) {
       resolve("Stuff worked")
     } else {
       reject("Oops there is a problem")
     }
   })
-  promise.then(result => result + '?').then(data => console.log(data))
-  const object = {
-    name:"Shubham",
-    lastName: "Patil"
-  }
+  promise.then(result => result + ' ?').then(data => console.log(data)).catch(error => console.log(error))
+  
   return(
     <div>
     <Freecodecamp/>
