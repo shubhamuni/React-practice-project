f9a5b67c1905bdee8343543c7e89929d2e6c8097
Shubham R Patil <shubhampatil.ani@gmail.com>
2024-05-07 08:00:27 +0530

    updating4

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -4,25 +4,17 @@ import Freecodecamp from "./components/Freecodecamp";
 
 function AppFunction () {
   const promise = new Promise((resolve, reject) => {
-    if(true)
+    if(true) {
+      resolve("Stuff worked")
+    } else {
+      reject("Oops there is a problem")
+    }
   })
+  promise.then(result => result + '?').then(data => console.log(data))
   const object = {
     name:"Shubham",
     lastName: "Patil"
   }
-  const fullName = function (city,state) {
-    console.log(this.name + " " + this.lastName + " from " + city+ " " + state)
-  }
-  fullName.call(object,"Pune","Maharashtra");
-  fullName.apply(object,["Pune","Maharashtra"])
-  const printMe = fullName.bind(object,"Pune","Maharashtra")
-  printMe()
-  const object1 = {
-    name:"Pratik",
-    lastName: "Sathye"
-  }
-  fullName.call(object1,"Malegaon","From call of maharashtra")
-  fullName.apply(object1,["Malegaon,","Maharashtra"])
   return(
     <div>
     <Freecodecamp/>
